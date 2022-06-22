08928b41b208e42ef47f9f4ad403a219b77e5974
Akinuliola Akinbobola <akinuliola_a@yahoo.com>
2022-06-22 21:53:56 +0100

    update

---
+++ script.js
@@ -0,0 +1,8 @@
+let fullName = "Akinuliola Akinbobola";
+let zuriId = "I4G000605O8F";
+let height = "5'10";
+let country = "Nigeria";
+
+alert(
+  `Hello, my name is ${fullName}. I am from ${country} and I am ${height} tall. I am a ${zuriId} student.`
+);
